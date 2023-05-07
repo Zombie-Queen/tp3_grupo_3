92b7ade7b7ea656708f2ee6d54fb722fa40ba8a2
sgchub <128912782+sgchub@users.noreply.github.com>
2023-05-07 13:17:15 -0300

    Update Genero.java
    Osvaldo

--- src/main/java/UTNFRGP/tp3_grupo_3/entidad/Genero.java
+++ src/main/java/UTNFRGP/tp3_grupo_3/entidad/Genero.java
@@ -1,4 +1,67 @@
 package UTNFRGP.tp3_grupo_3.entidad;
 
-public class Genero {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name="Genero")
+public class Genero implements Serializable {
+
+@Id
+@Column
+@GeneratedValue(strategy = GenerationType.AUTO)
+private int idgenero;
+
+@Column
+private String descripción;
+
+@ManyToMany(cascade= (CascadeType.ALL))
+private HashSet listaLibros= new HashSet;
+
+@JoinTable(name="Libros_x_genero",joinColumns= {@JoinCoumn(name="idgenero")}, inverseJoinColumn= {@JoinColumn(name="idlibro")})
+
+public Genero()
+{
+
+}
+
+
+public HashSet getListaLibros() {
+return listaLibros;
+}
+
+
+public void setListaLibros(HashSet listaLibros) {
+this.listaLibros = listaLibros;
+}
+
+
+public String getDescripción() {
+return descripción;
+}
+
+public void setDescripción(String descripción) {
+this.descripción = descripción;
+}
+
+
+@Override
+public String toString() {
+return "Genero [idgenero=" + idgenero + ", descripción=" + descripción + "]";
+}
+
+
+
+}
+
+
+
 }
